d99474ef087630eb6225bc494c4afccb54237b1d
lilru2 <lilru2kyuu@protonmail.ch>
2020-01-13 19:47:49 +1300

    Implement client Socket.IO connections

--- server.py
+++ server.py
@@ -3,12 +3,34 @@ import os
 import eventlet
 import socketio
 
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#                                                                        SETUP
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 static_files = {
-    '/': 'pages/index.html'
+    '/': 'pages/index.html',
+    '/js/client.js': 'public/js/client.js'
 }
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio, static_files=static_files)
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#                                                                    SOCKET.IO
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+@sio.on('connect')
+def connect(sid, env):
+    print(f'Connected: {sid}')
+
+@sio.on('disconnect')
+def disconnect(sid):
+    print(f'Disconnected: {sid}')
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#                                                                          RUN
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 if __name__ == '__main__':
     if 'PORT' in os.environ.keys():
